b1f809ee394240a2e6e1d6ed4ce45b097510d021
julkute02 <straznickaitejulita@gmail.com>
2017-03-21 11:08:21 +0100

    first javascript example

---
+++ main.js
@@ -0,0 +1,17 @@
+let partner ="Sam"
+
+let name = "Julita"
+
+let job="Coder";
+
+let numkIDS= "1";
+
+let address= "Skindergrade";
+
+
+numkIDS = 1;
+
+let age = 23;
+let info = job + "in" + address + "and married to" +partnername"width"+nameKids+"kinds"
+
+consule.log()
